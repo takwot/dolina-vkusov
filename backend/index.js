5fdfe1b2b761df705606863a8d13c56443856594
Roman <ketzalec@gmail.com>
2023-07-17 17:25:26 +0300

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/store", {
+  .connect("mongodb://database:27017/store", {
     useNewUrlParser: true,
   })
   .then(() => console.log("MongoDB Connected"))
